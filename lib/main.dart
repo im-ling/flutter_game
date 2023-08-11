e3ce38e87dc95e5749077b8abff7279f52286e34
ling <tobeabetterman_dl@163.com>
2023-08-11 16:29:12 +0800

    feat: 列表页

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,83 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const IndexPage(),
+        initialRoute: "/",
+        routes: {
+          // 静态页面
+          // "/": (context) => const IndexPage(),
+          "/jump_game": (context) => const JumpGamePage(),
+          "/snake_game": (context) => const SnakeGmePage(),
+        });
+  }
+}
+
+class IndexPage extends StatefulWidget {
+  const IndexPage({super.key});
+
+  @override
+  State<IndexPage> createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(title),
+      // ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.grey[300],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Index",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                // color: Colors.blueAccent,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/snake_game', arguments: {});
+              },
+              child: const Text("Snake Game"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/jump_game', arguments: {});
+              },
+              child: const Text("Jump Game"),
+            )
+          ],
+        ),
       ),
-      home: const SnakeGmePage(),
     );
   }
 }
