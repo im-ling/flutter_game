55d1ee30d3bb0fc7bb93e22fad4aca2d520b583a
ling <tobeabetterman_dl@163.com>
2023-10-07 20:13:48 +0800

    feat: tmp

--- lib/flip_card.dart
+++ lib/flip_card.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FlipCardPage extends StatefulWidget {
@@ -8,10 +10,82 @@ class FlipCardPage extends StatefulWidget {
 }
 
 class _FlipCardPageState extends State<FlipCardPage> {
+  String title = "Flip Card";
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pinkAccent,
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(title),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: FlipCard(),
+      ),
+    );
+  }
+}
+
+class FlipCard extends StatefulWidget {
+  const FlipCard({
+    super.key,
+  });
+
+  @override
+  State<FlipCard> createState() => _FlipCardState();
+}
+
+class _FlipCardState extends State<FlipCard> {
+  bool isFront = true;
+
+  void testFunction() {
+    print("test");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isFront = !isFront;
+        });
+      },
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 500),
+        child: isFront
+            ? Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+                child: const Text(
+                  "front",
+                  style: TextStyle(color: Colors.white, fontSize: 32),
+                ),
+              )
+            : Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: Colors.greenAccent,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+                child: const Text(
+                  "back",
+                  style: TextStyle(color: Colors.white, fontSize: 32),
+                ),
+              ),
+      ),
     );
   }
 }
