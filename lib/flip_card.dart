8dc537c7b4d14e7809fd79e19a137d90a4a38279
ling <tobeabetterman_dl@163.com>
2023-10-17 08:09:03 +0800

    feat: 16个card 布局

--- lib/flip_card.dart
+++ lib/flip_card.dart
@@ -11,8 +11,19 @@ class FlipCardPage extends StatefulWidget {
 
 class _FlipCardPageState extends State<FlipCardPage> {
   String title = "Flip Card";
+  List<String> nums = List.generate(16, (index) => index.toString());
+
   @override
   Widget build(BuildContext context) {
+    // double width = MediaQuery.of(context).size.width;
+    // double height = MediaQuery.of(context).size.height;
+    // var padding = MediaQuery.of(context).padding;
+    // double newheight = height - padding.top - padding.bottom;
+    // var cardSideLength = max(width, newheight) / 4.0 - 10;
+    double cardSideLength = 80;
+    double interval = 10;
+    // var result = cardSideLength;
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -20,18 +31,28 @@ class _FlipCardPageState extends State<FlipCardPage> {
         title: Text(title),
       ),
       body: Container(
+        alignment: Alignment.center,
         height: double.infinity,
         width: double.infinity,
-        child: FlipCard(),
+        child: SizedBox(
+          width: cardSideLength * 4 + interval * 3,
+          height: cardSideLength * 4 + interval * 3,
+          child: Wrap(
+            spacing: interval,
+            runSpacing: interval,
+            children:
+                nums.map((e) => FlipCard(sideLen: cardSideLength)).toList(),
+          ),
+        ),
       ),
     );
   }
 }
 
 class FlipCard extends StatefulWidget {
-  const FlipCard({
-    super.key,
-  });
+  FlipCard({super.key, required this.sideLen});
+
+  double sideLen;
 
   @override
   State<FlipCard> createState() => _FlipCardState();
@@ -61,22 +82,27 @@ class _FlipCardState extends State<FlipCard> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          isFront = !isFront;
-        });
-      },
-      child: AnimatedSwitcher(
-        transitionBuilder: _transitionBuilder,
-        duration: const Duration(milliseconds: 1000),
-        child: isFront
-            ? MyCard(
-                key: ValueKey(true),
-                str: "正面",
-                color: Colors.blue,
-              )
-            : MyCard(key: ValueKey(false), str: "", color: Colors.green),
+    return SizedBox(
+      width: widget.sideLen,
+      height: widget.sideLen,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            isFront = !isFront;
+          });
+        },
+        child: AnimatedSwitcher(
+          transitionBuilder: _transitionBuilder,
+          duration: const Duration(milliseconds: 1000),
+          child: isFront
+              ? const MyCard(
+                  key: ValueKey(true),
+                  str: "front",
+                  color: Colors.blue,
+                )
+              : const MyCard(
+                  key: ValueKey(false), str: "", color: Colors.green),
+        ),
       ),
     );
   }
@@ -96,8 +122,6 @@ class MyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       key: key,
-      width: 200,
-      height: 200,
       alignment: Alignment.center,
       decoration: BoxDecoration(
         color: color,
@@ -107,7 +131,7 @@ class MyCard extends StatelessWidget {
       ),
       child: Text(
         str,
-        style: const TextStyle(fontSize: 32),
+        style: const TextStyle(fontSize: 20),
       ),
     );
   }
